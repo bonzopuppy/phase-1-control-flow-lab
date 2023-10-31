0034d1e431ef18da6708f98377a96722830b8de1
David Baden <davebaden@gmail.com>
2023-10-30 20:45:11 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let result;
+  if (ride<=400) {
+    result = 'This one is on me!';
+  } else if (ride>200 && ride<=2000) {
+    result = 'That will be twenty bucks.'
+  } else if (ride>2000 && ride<=2500) {
+    result = 'I will gladly take your thirty bucks.'
+  } else {
+    result = "No can do."
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ const result = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+ return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    default:
+      return 'Bye.';
+      break;
+  }
 }
